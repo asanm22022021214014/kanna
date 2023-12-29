b83287b4f4ebbecd53002bd8f823502976a67467
asanm22022021214014 <148636823+asanm22022021214014@users.noreply.github.com>
2023-12-29 17:58:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>collage</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+<body>
+    <div class="menu-bar">
+    <ul>
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Program</a>
+           <div class="sub-menu-1">
+              <ul>
+                
+                <li><a href="#">Deportment</a></li>
+                <li><a href="#">Stafs</a></li>
+                <li><a href="form.php">Registration form</a></li>
+              </ul>
+            </div>
+        </li>
+        <li><a href="#"><i class= "#"></i> student</a>
+            <div class="sub-menu-1">
+                <ul>
+                    <li class="hover-me"><a href="#">B.Sc Computer Science</a><i class="angle-right"></i>
+                       <div class="sub-menu-2">
+                        <ul>
+                            <li><a href="display1cs.php">I-CS</a></li>
+                             <li><a href="#">II-CS</a></li>
+                            <li><a href="display.php">III-CS</a></li>
+                        </ul>                        
+                       </div>
+                    
+                    </li>
+                    <li class="hover-me"><a href="#">BCA</a><i class="fa fa-angle-right"></i>
+                        <div class="sub-menu-2">
+                        <ul>
+                            <li><a href="#">I-BCA</a></li>
+                             <li><a href="#">II-BCA</a></li>
+                            <li><a href="#">III-BCA</a></li>
+                        </ul>                        
+                       </div>
+                    
+                    </li>
+                    <li class="hover-me"><a href="#">B.COM</a><i class="fa fa-angle-right"></i>
+                        <div class="sub-menu-2">
+                        <ul>
+                            <li><a href="#">I-B.com</a></li>
+                             <li><a href="#">II-B.com</a></li>
+                            <li><a href="#">III-B.com</a></li>
+                        </ul>                        
+                    </div>
+                    </li>
+                    <li class="hover-me"><a href="#">MATHS</a><i class="fa fa-angle-right"></i>
+                        <div class="sub-menu-2">
+                        <ul>
+                            <li><a href="#">I-Maths</a></li>
+                             <li><a href="#">II-Maths</a></li>
+                            <li><a href="#">III-Maths</a></li>
+                        </ul>                        
+                       </div>
+                    </li>
+                    <li class="hover-me"><a href="#">ENGLISH</a><i class="fa fa-angle-right"></i>
+                       <div class="sub-menu-2">
+                        <ul>
+                            <li><a href="#">I-English</a></li>
+                             <li><a href="#">II-English</a></li>
+                            <li><a href="#">III-English</a></li>
+                        </ul>                        
+                       </div>
+                    
+                    </li>
+                 </ul>
+            </div>
+         </li>
+        <li><a href="#">Admission</a></li>
+        <li><a href="#">Contect</a></li>
+    </ul>
+    <section class="home">
+        <div class="home-content">
+            <h1>HI i am kanna </h1>
+            <h3>NATIONAL COLLEGE OF ARTS AND SCIENCE </h3>            
+            <h2>Affilitated by Annamalai University</h2>
+            <h4>Avanipur Road ,Thindivanam- 604307.</h4>
+        </div>
+    </section>
+</div>
+</body>
+</html>
